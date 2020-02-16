56853051ad1ff9fdb01d0ddd2bc1e6d8d386f4df
aishwariya2012 <aishwariyapradeepbhai.bhatt@sjsu.edu>
2020-02-16 13:40:36 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-/**
- * http://usejsdoc.org/
- */
+
 var express = require('express');
 var router = express.Router();
-// var ctrlMain = require("../controllers/main");
 
 
-/*
- * GET Login page.
- */
+
+
 router.get('/', function(req, res)
 {
 	res.render('../views/login', { title: ''});
 });
 
-// /*
-//  * GET home page.
-//  */
+
  router.post('/foodhome', function(req,res){
  if(req.body.Logout){
     res.render('../views/login', { title: ''});
@@ -28,14 +22,6 @@ else if(req.body.username === 'user' && req.body.password === 'password') {
 }
  });
 
-// /*
-//  * GET restaurant page.
-//  */
-// router.post('/restaurant', ctrlMain.restaurant);
 
-// /*
-//  * GET billing page.
-//  */
-// router.post('/billing', ctrlMain.billing);
 
 module.exports = router;
