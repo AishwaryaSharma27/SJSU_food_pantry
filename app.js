8aefae626f49f3fed77222801a33938b450119b8
aishwariya2012 <aishwariyapradeepbhai.bhatt@sjsu.edu>
2020-02-16 14:16:50 -0800

    Added Images of Zones

--- app.js
+++ app.js
@@ -1,34 +1,4 @@
-// var express = require('express');
-// var app = express();
 
-// var bodyParser = require("body-parser");
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// app.get('/', function (req, res) {
-//     res.sendFile('/Users/abhatt/Documents/GitHub/SJSU_food_pantry/index.html');
-// });
-
-// app.post('/submit-user-data', function (req, res) {
-
-//     if(req.body.firstName === 'user' && req.body.lastName === 'password'){
-//         var name = req.body.firstName + ' ' + req.body.lastName;
-//         res.sendFile('/Users/abhatt/Documents/GitHub/SJSU_food_pantry/submit.html');
-//     }
-//     else{
-//         res.send('Wrong username or password');
-//     }
-  
-//     // res.send(name + ' Submitted Successfully!');
-// });
-
-
-// var server = app.listen(3008, function () {
-//     console.log('Node server is running..');
-// });
-
-/**
- * http://usejsdoc.org/
- */
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
