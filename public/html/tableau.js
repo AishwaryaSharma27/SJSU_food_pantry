aeb1ecaa7a0c43f5d5c7415491fe3f35d9a46a8c
Aishwariya <bhattaishwariya@gmail.com>
2020-05-06 20:32:20 -0700

    new links of dashboard

--- public/html/tableau.js
+++ public/html/tableau.js
@@ -1,8 +1,8 @@
 function initViz() {
     var containerDiv = document.getElementById("vizContainer"),
-    url = "https://public.tableau.com/views/Main_dashboard/MainDashboard?:display_count=y&:origin=viz_share_link";
+    url = "https://public.tableau.com/views/Main_dashboard_15888126425390/MainDashboard?:display_count=y&:origin=viz_share_link";
     options = {
-        width: '1200px',
+        width: '1500px',
         height: '800px',
           hideTabs: false,
                               hideToolbar: true,
